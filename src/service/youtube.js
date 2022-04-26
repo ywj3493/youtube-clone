cad05ee31a1e88f2e825870d7b4f4d000127d958
ywj3493 <ywj3493@nate.com>
2022-04-26 11:39:10 +0900

    20220426

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -5,36 +5,34 @@ export default class Youtube {
     this.key = key;
   }
 
-  search(q, callback) {
+  async search(q, callback) {
     let config = {
       method: "get",
       url: `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${q}&type=video&key=${this.key}`,
       headers: {},
     };
 
-    return axios(config)
-      .then(function (response) {
-        console.dir(JSON.stringify(response.data.items.length));
-        callback(response.data.items);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    try {
+      const response = await axios(config);
+      console.dir(JSON.stringify(response.data.items.length));
+      callback(response.data.items);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  mostPopular(callback) {
-    var config = {
+  async mostPopular(callback) {
+    let config = {
       method: "get",
       url: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
       headers: {},
     };
 
-    return axios(config)
-      .then(function (response) {
-        callback(response.data.items);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    try {
+      const response = await axios(config);
+      callback(response.data.items);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
